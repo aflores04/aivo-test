574fa12affdaf4c8bb8d03bd724b1a66c0290e30
Alan Flores <alan@25watts.com.ar>
2018-11-25 16:32:15 -0300

    basic structure

---
+++ test/RoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class RoutesTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * @test
+     */
+    public function assertExample()
+    {
+        $this->assertTrue(true);
+    }
+
+}
